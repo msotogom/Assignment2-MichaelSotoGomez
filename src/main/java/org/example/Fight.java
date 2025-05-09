42ba458410eacaa91059d484b1ed9a456fdf777f
msotogom <msotogom@depaul.edu>
2025-05-09 02:42:40 -0500

    Fixed code to continue the fight until their is a victor.

--- src/main/java/org/example/Fight.java
+++ src/main/java/org/example/Fight.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Random;
 
 /**
- * Action that allows the player to fight a monster at a door
+ * Action that lets the player fight a monster guarding a door.
  */
 public class Fight implements Action {
     private Character player;
@@ -12,9 +12,10 @@ public class Fight implements Action {
 
     /**
      * Constructs a Fight action.
-     * @param player  the player figthing
-     * @param monster the monster being fought
-     * @param door    the door the monster is guarding
+     *
+     * @param player  the player
+     * @param monster the monster
+     * @param door    the door being guarded
      */
     public Fight(Character player, Monster monster, Door door) {
         this.player = player;
@@ -23,37 +24,48 @@ public class Fight implements Action {
     }
 
     /**
-     * rolls combat scores, applies damage, and checks if the monster is defeated.
+     * Executes a full fight loop between the player and monster.
      */
     @Override
     public void execute() {
         boolean useStrength = monster.getCraft() == 0;
-        int playerScore = player.rollCombatScore(useStrength);
-        int monsterScore = monster.rollCombatScore();
 
-        System.out.println("Fight begins! You rolled " + playerScore + ". Monster rolled " + monsterScore + ".");
+        System.out.println("Combat begins against " + monster.getName() + "!");
 
-        int damage = Math.abs(playerScore - monsterScore);
-        if (playerScore > monsterScore) {
-            monster.takeDamage(damage);
-            System.out.println("You hit the monster for " + damage + " damage!");
-            if (!monster.isAlive()) {
-                door.clearMonster();
-                System.out.println("The monster is defeated!");
+        while (player.isAlive() && monster.isAlive()) {
+            int playerScore = player.rollCombatScore(useStrength);
+            int monsterScore = monster.rollCombatScore();
+
+            System.out.println("You rolled: " + playerScore);
+            System.out.println(monster.getName() + " rolled: " + monsterScore);
+
+            int damage = Math.abs(playerScore - monsterScore);
+            if (playerScore > monsterScore) {
+                monster.takeDamage(damage);
+                System.out.println("You hit the monster for " + damage + " damage!");
+            } else if (monsterScore > playerScore) {
+                player.takeDamage(damage);
+                System.out.println("The monster hits you for " + damage + " damage!");
+            } else {
+                System.out.println("Both attacks miss!");
             }
-        } else {
-            player.takeDamage(damage);
-            System.out.println("The monster damages you for " + damage + " damage!");
-            if (!player.isAlive()) {
-                System.out.println("You have been defeated.");
-            }
+
+            System.out.println("Your health: " + player.getHealth());
+            System.out.println(monster.getName() + "'s health: " + monster.getHealth());
+            System.out.println();
+        }
+
+        if (!monster.isAlive()) {
+            door.clearMonster();
+            System.out.println("You defeated the monster!");
+        }
+
+        if (!player.isAlive()) {
+            System.out.println("You have been defeated.");
         }
     }
 
-    /**
-     * Describes fight
-     * @return fight string
-     */
+
     @Override
     public String toString() {
         return "Fight monster: " + monster.getName();
