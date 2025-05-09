b9fe600880141f669220fe3d96b0072eb10867a7
msotogom <msotogom@depaul.edu>
2025-05-09 02:39:26 -0500

    Fixes bug in move that allows player to move past door with monster

--- src/main/java/org/example/Move.java
+++ src/main/java/org/example/Move.java
@@ -1,7 +1,7 @@
 package org.example;
 
 /**
- * Action that moves the player to another chamber
+ * Action that moves the player to another chamber through a door,
  */
 public class Move implements Action {
     private Dungeon dungeon;
@@ -9,7 +9,9 @@ public class Move implements Action {
 
     /**
      * Constructs a Move action.
-     * @param door    the door to move through
+     *
+     * @param dungeon the dungeon
+     * @param door    the door
      */
     public Move(Dungeon dungeon, Door door) {
         this.dungeon = dungeon;
@@ -17,17 +19,26 @@ public class Move implements Action {
     }
 
     /**
-     * Executes the move action by updating the player's current chamber.
+     * Executes the move action. Player can only move if the door is unguarded or the monster fight is won
      */
     @Override
     public void execute() {
+        Monster monster = door.getMonster();
+        if (monster != null && monster.isAlive()) {
+            System.out.println("You can't move through this door. It's still guarded by a monster!");
+            return;
+        }
+
         Chamber current = dungeon.getCurrentChamber();
         Chamber next = door.getOtherChamber(current);
         dungeon.setCurrentChamber(next);
         System.out.println("You moved into the next chamber.");
     }
 
-
+    /**
+     * Describes this action.
+     * @return action name
+     */
     @Override
     public String toString() {
         return "Move through door";
