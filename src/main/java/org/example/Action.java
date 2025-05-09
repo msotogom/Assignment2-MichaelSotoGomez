cd1b5b476f3aa30029510bdeff1bd0a26e91fe3f
msotogom <msotogom@depaul.edu>
2025-05-09 01:31:24 -0500

    Interface for moving, picking up item or fighting.

---
+++ src/main/java/org/example/Action.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * Interface for all player actions
+ */
+public interface Action {
+    /**
+     * Executes the moving, picking up item or fighting.
+     */
+    void execute();
+}
